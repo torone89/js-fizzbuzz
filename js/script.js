a7ca8b4556651b0a46282edf68b124dde4b8f8f4
torone89 <torone89>
2022-06-23 14:28:42 +0200

    Trovato calcolo multipli

--- js/script.js
+++ js/script.js
@@ -3,8 +3,36 @@
 console.log("JS")
 
 
-// SCRIVO UN NUMERO DA 1 a 100
+// // SCRIVO UN NUMERO DA 1 a 100
 
-for (let i = 1; i <= 100; i++) {
-    console.log("scrivi " + i)
+// let content = ''; contaMultipi = 0
+// for (let i = 1; i <= 100; i++) {
+//     content = ("scrivi " + i);
+//     console.log(content);
+
+
+
+
+let prova
+
+// for (var i = 0; i < 100; i++) {
+
+//     prova = i
+//     if (prova % 3 == 0) {
+//         prova = "Fizz"
+//     }
+//     console.log(prova)
+// }
+
+
+
+
+for (var i = 0; i < 100; i++) {
+
+    prova = i
+    if (prova % 5 == 0) {
+        prova = "buzz"
+    }
+    console.log(prova)
 }
+
